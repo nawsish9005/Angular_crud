9be5fcbbcc6952a5f7efefff46b2b59e6ea71f48
nawsish <nawsish2018@gmail.com>
2025-03-13 15:16:10 +0600

    add form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './core/components/navbar/navbar.component';
+import { CreatePostComponent } from "./features/post/pages/create-post/create-post.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [RouterOutlet, NavbarComponent, CreatePostComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
