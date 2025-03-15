982cc32e33229d1c7806b8882d6fc59cd335af69
nawsish <nawsish2018@gmail.com>
2025-03-15 13:48:00 +0600

    add form

--- src/app/features/post/pages/create-post/create-post.component.ts
+++ src/app/features/post/pages/create-post/create-post.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-create-post',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './create-post.component.html',
   styleUrl: './create-post.component.css'
 })
 export class CreatePostComponent {
+  createPostForm = new FormGroup({
+    title: new FormControl<string>('', {nonNullable: true, validators: [Validators.required, Validators.minLength(6), Validators.maxLength(100)]}),
+    content: new FormControl<string>('', {nonNullable: true, validators: [Validators.required, Validators.maxLength(3000)]}), 
+  });
 
+  onFormSubmit(){
+    console.log(this.createPostForm.value);
+  }
 }
